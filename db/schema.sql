a98f8036df0e69b8be62a21f35f565ea2365bf85
thomromano <thomromano@gmail.com>
2018-05-17 15:19:05 -0500

    setup connection to mysql in connection.js

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
+-- source C:/Users/DeGri/Desktop/burger/db/schema.sql
 DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
-
 CREATE TABLE burgers (
-id INTEGER(11) AUTO_INCREMENT NOT NULL,
-burger_name VARCHAR(255) NOT NULL,
-devoured BOOLEAN NOT NULL,
-PRIMARY KEY (id)
+    id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN NOT NULL,
+    PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
+-- source C:/Users/DeGri/Desktop/burger/db/seeds.sql
 INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Bison Burger', FALSE);
