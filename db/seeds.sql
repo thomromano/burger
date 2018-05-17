839e87ad0d25fe295d3ee8707b1fab405d61b4ec
thomromano <thomromano@gmail.com>
2018-05-17 00:45:00 -0500

    add insert queries to seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bison Burger', FALSE);
