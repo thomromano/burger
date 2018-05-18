cea055e338529508b8c783011166bf4a47e0be38
thomromano <thomromano@gmail.com>
2018-05-18 12:40:49 -0500

    add to orm.js

---
+++ config/orm.js
@@ -0,0 +1,36 @@
+var connection = require("./connection.js");
+
+
+function printQuestionMarks(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
+var orm = {
+    all: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    };
+
+    create: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+    }
+}
+
