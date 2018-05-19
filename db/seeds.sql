41524cd0cd8a05ff1aaa7396050920db73d3ea31
thomromano <thomromano@gmail.com>
2018-05-19 18:35:19 -0500

    seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,6 @@
--- source C:/Users/DeGri/Desktop/burger/db/seeds.sql
+-- sou
+
+rce C:/Users/DeGri/Desktop/burger/db/seeds.sql
 INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Bison Burger', FALSE);
