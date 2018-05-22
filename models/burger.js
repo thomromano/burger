61093bcb8b56e7151a2fca1bfdd2b1a00792fa93
thomromano <thomromano@gmail.com>
2018-05-22 15:53:49 -0500

    run burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,28 @@
 //import orm.js into burger.js
 //create the code that will call the ORM functions using burger specific input for the ORM
 //Export at the end of the burger.js file
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 8000;
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgersController.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("Listening on port: " + PORT);
+});
