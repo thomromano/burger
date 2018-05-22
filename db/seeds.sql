9542736efd6582c92fa800338713f42b8b8495b2
thomromano <thomromano@gmail.com>
2018-05-21 23:52:55 -0500

    burger_controller.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,4 @@
--- sou
-
-rce C:/Users/DeGri/Desktop/burger/db/seeds.sql
+-- source C:/Users/DeGri/Desktop/burger/db/seeds.sql
 INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Bison Burger', FALSE);
